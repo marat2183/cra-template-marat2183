99cbe333ead41b1a043105bab437296869b98853
Marat Grigoryan <marat.grig318@gmail.com>
2022-08-31 14:57:32 +0300

    initial commit

---
+++ template/src/App.js
@@ -0,0 +1,11 @@
+import Index from 'pages/index';
+
+import './App.scss';
+
+function App() {
+  return (
+    <Index/>
+  );
+}
+
+export default App;
---
+++ template/src/pages/index/index.js
@@ -0,0 +1,8 @@
+import s from './index.module.scss';
+
+const Index = () => {
+  return (
+    <h1>Index Page</h1>
+  );
+};
+export default Index;
